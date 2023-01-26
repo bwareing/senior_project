fc29a94df05f7a29a8bc0cd618a4d92289402dbf
Brandon Wareing <BDWareing@gmail.com>
2023-01-26 15:08:49 -0700

    made mainpage go to my story

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const AppBarApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppBarApp extends StatelessWidget {
+  const AppBarApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: AppBarExample(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Coulnser outreach'),
+        actions: <Widget>[
+          
+          IconButton(
+            icon: const Icon(Icons.navigate_next),
+            tooltip: 'Our story',
+            
+          
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                   
+                      title: const Text('Our story'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        ' I was inspired to make this platform during a pretty hard time in my life. Me and my wife were\n'
+                        'trying to get pregant with our first child. After 6 months of unprotected sex and no luck we decied\n'
+                        'it was time to get checked out. So we went to a obgyn to get my wife checked out. The obgyn did not \n'
+                        'see anything out of the ordinary. He ask if she had regualer periods she then let him know that it was\n'
+                        'regualer. He gave her meds that made her have regualer cycle, I thought we were in the allclear. We then\n '
+                        'had unprotected for a other six months. No luck we went back to the obgyn. He then had my wife do a\n '
+                        'ultrasound evrything looked good. He then said what I was dreading it was time to test me. I was fine when\n'
+                        'The problem was here because I could control how I reacted but could not control how she reacted if it was\n'
+                        'my issue.\n '
+                        ' I did a sperm test and my counts was low. It was a crushing blow and I just had a fear of my wife leaving me.\n'
+                        'My wife told me that we would make it through it and we were a team. These kind words helped my fear go down but\n'
+                        'in the back of my mind I was still afaird of her leaving me. By this point my wife was trying to be strong but there\n'
+                        'were nights she was so depressed, even to the point of crying. In my mind I would tell myself that every tear she shead\n'
+                        'was my fault that I was hurting my wife.\n'
+                        ' I then went into a dark place. I started to become really withdrawn from my wife. I did not want her to know how much\n'
+                        'I was struggling, I thought I needed to be strong for her. After a couple months I started to have a thought if I died\n'
+                        'she could marry someone who could give her children. That thought then turned into I wish I would die so she could marry\n'
+                        'someone else. At this point I woke up every morning I woke up racked with guilt she was married to me. The thought of ended\n'
+                        'my life did cross my mind. I was to scared to but was hopping I would go to sleep and not wake up. My wife notice the change\n'
+                        'in me and conforted me. She demanded I told her what the heck was going on. I broke and told her everything. I then saw a\n'
+                        'look on her face that periced me straight to the core, pure fear.  Since then I have got the support I needed from family\n'
+                        'to help me get out of that deep depression. This made me want every body to get the mental health that they need regradless\n'
+                        'if they do not have the money to pay for mental health services.',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: const Center(
+        child: Text(
+          'This is the home page',
+          style: TextStyle(fontSize: 24),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
