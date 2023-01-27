c26cfb3aa847843aae69df52a25fc4e8e81f2aa3
Brandon Wareing <BDWareing@gmail.com>
2023-01-27 15:38:42 -0700

    added a button and one textfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/views/screens/homepage.dart';
-import 'main.dart';
+
 
 void main(){
   runApp(Counsler());
--- lib/views/screens/homepage.dart
+++ lib/views/screens/homepage.dart
@@ -9,7 +9,30 @@ class Loginin extends StatefulWidget{
     @override
     Widget build(BuildContext context){
       return Scaffold(
+        appBar: AppBar(
+          title: const Text('Counsler OutReach'),
+          elevation: 0,
+          backgroundColor: Color.fromARGB(255, 59, 77, 86),
 
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 0.5 * MediaQuery.of(context).size.width,
+              child:TextField()),
+              
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+              ElevatedButton(onPressed:() {} , child: Text('Log in')),
+              ],
+            ),  
+
+          ],
+        ),
       );
     }
   }
